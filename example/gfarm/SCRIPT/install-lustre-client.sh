cfc42d9fb79e1e5ece67f949d301fc9169aa7363
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2024-02-09 10:25:24 +0900

    SCRIPT/install-lustre-client.sh

---
+++ example/gfarm/SCRIPT/install-lustre-client.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source /SCRIPT/lib.sh
+
+cat <<EOF | SUDO tee /etc/yum.repos.d/lustre.repo
+[lustre-client]
+name=lustre-client
+#baseurl=http://metaserver/repo/lustre-client
+baseurl=https://downloads.whamcloud.com/public/lustre/latest-release/el8.9/client
+enabled=0
+gpgcheck=0
+
+[e2fsprogs-wc]
+name=e2fsprogs-wc
+#baseurl=http://metaserver/repo/e2fsprogs-wc
+baseurl=https://downloads.whamcloud.com/public/e2fsprogs/latest/el8
+enabled=0
+gpgcheck=0
+EOF
+
+SUDO yum --nogpgcheck --enablerepo=e2fsprogs-wc upgrade -y \
+     e2fsprogs
+SUDO yum --nogpgcheck --enablerepo=lustre-client install -y \
+     kmod-lustre-client lustre-client
