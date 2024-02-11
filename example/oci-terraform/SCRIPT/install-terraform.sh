9904cf01933ca4bdcf2c2827c9739177184c1419
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2024-02-12 04:44:32 +0900

    install terraform, and setup OCI API

---
+++ example/oci-terraform/SCRIPT/install-terraform.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu
+set -x
+
+sudo apt install -y gnupg
+
+KEYRING=hashicorp-archive-keyring.gpg
+wget -O- https://apt.releases.hashicorp.com/gpg \
+    | gpg --dearmor > $KEYRING
+
+FP="798A EC65 4E5C 1542 8C8E  42EE AA16 FCBC A621 E701"
+#gpg --no-default-keyring --keyring ./${KEYRING} --fingerprint | grep "$FP"
+gpg --no-default-keyring --keyring ./${KEYRING} --fingerprint | grep "798A"
+
+sudo cp -f $KEYRING /usr/share/keyrings/${KEYRING}
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+sudo apt-get install -y terraform
+terraform -install-autocomplete
---
+++ example/oci-terraform/SCRIPT/setup-oci-api.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -eu
+set -x
+
+# SEE: https://oracle-japan.github.io/ocitutorials/intermediates/terraform/
+
+PUBKEY=/CONF/admin_public.pem
+PRIVKEY=/CONF/admin_private.pem
+
+if [ -f $PRIVKEY ]; then
+    cat $PUBKEY
+    exit 0
+fi
+
+# 1. ociディレクトリの作成
+#mkdir $HOME/.oci
+
+# 2. 秘密鍵の作成　<your-rsa-key-name>には適当な名前を入力
+openssl genrsa -out $PRIVKEY 2048
+
+# 3. 権限の変更
+chmod 600 $PRIVKEY
+
+# 4. 公開キーの作成
+openssl rsa -pubout -in $PRIVKEY -out $PUBKEY
+
+# 5. 公開キーをコンソールに登録
+cat $PUBKEY
+
+# 6. アイデンティティ -> 自分のプロファイル -> APIキー -> 公開キーの追加
+
+# 7. 公開キーの貼付け -> EGIN PUBLIC KEYおよびEND PUBLIC KEYの行を含む値を貼り付け -> 追加
+
+
+# SSH 公開鍵
+cd /CONF
+if [ ! -f sshkey ]; then
+    ssh-keygen -t rsa -N "" -b 2048 -C sshkey -f sshkey
+fi
