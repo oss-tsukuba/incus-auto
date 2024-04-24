51bcf0117f846f3cb5e308a4524decefe20fc2f4
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2024-04-24 23:17:45 +0900

    example/gfarm-terraform-oci: move "install ansible" from cloud-init to ansible-init.sh

--- example/gfarm-terraform-oci/terraform/ansible/ansible-init.sh
+++ example/gfarm-terraform-oci/terraform/ansible/ansible-init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 source ./ansible-common.sh
+
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y --update ppa:ansible/ansible
+sudo apt-get install -y ansible
+
 ansible-playbook -i $INV ./playbook-ssh-keyscan.yaml
 ansible-playbook -i $INV ./playbook-ssh-send-privkey.yaml
