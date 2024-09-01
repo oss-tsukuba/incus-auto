046f204123900d4b43172bf1683cb60ecf0b4e16
takuya-isbs <57710912+takuya-isbs@users.noreply.github.com>
2024-09-01 17:25:54 +0900

    example/gfarm:hsm-start.sh: use runuser instead of sudo

--- example/gfarm/SCRIPT/hsm-start.sh
+++ example/gfarm/SCRIPT/hsm-start.sh
@@ -6,19 +6,24 @@ set -x
 MNT_LUSTRE=/mnt/lustre
 GFS_MOUNTDIR=/mnt/gfarm-lustre-hsm
 GFARMFS_ROOT=/LustreHSM
+GFS_USERNAME=gfarmsfs  # global user in Gfarm
+GFARMFS_USERNAME=gfarmsys  # local user
 HSM_ROOT=${GFS_MOUNTDIR}
 LUSTRE_SRC_ORIG=/SRC/lustre-release
-LUSTRE_SRC=${HOME}/lustre-release
+LUSTRE_SRC=/home/${GFARMFS_USERNAME}/lustre-release
 HSM_POSIX_DIR=${LUSTRE_SRC}/hsm-posix
-ARCHIVE=1
+
+HSM_ARCHIVE=1
+
+#HSM_OPTIONS="--no-attr"
+HSM_OPTIONS=""
 
 MOUNT_GFARM2FS_HSMTOOL=${HSM_POSIX_DIR}/mount-gfarm2fs-and-lhsmtool_posix.sh
 HSMTOOL=${HSM_POSIX_DIR}/lhsmtool_posix
 
-#DAEMON=--daemon
-DAEMON=
+RUN="runuser -u $GFARMFS_USERNAME --"
 
-rsync -av --delete $LUSTRE_SRC_ORIG/ $LUSTRE_SRC/
+$RUN rsync -av --delete $LUSTRE_SRC_ORIG/ $LUSTRE_SRC/
 
 if [ ! -e "$HSM_POSIX_DIR" ]; then
     cat <<EOF
@@ -28,13 +33,13 @@ EOF
     exit 1
 fi
 
-[ -f "$HSMTOOL" ] || (cd "$HSM_POSIX_DIR" && ./build.sh)
+[ -f "$HSMTOOL" ] || (cd "$HSM_POSIX_DIR" && $RUN ./build.sh)
 
 BACKUP_RESTORE /etc/fuse.conf
-echo "user_allow_other" | SUDO tee -a /etc/fuse.conf
+echo "user_allow_other" | tee -a /etc/fuse.conf
 
-SUDO mkdir -p $GFS_MOUNTDIR
-SUDO chmod 700 $GFS_MOUNTDIR
-SUDO chown `whoami` $GFS_MOUNTDIR
+mkdir -p $GFS_MOUNTDIR
+chmod 700 $GFS_MOUNTDIR
+chown $GFARMFS_USERNAME $GFS_MOUNTDIR
 
-sh -x "$MOUNT_GFARM2FS_HSMTOOL" "$GFS_MOUNTDIR" "$GFARMFS_ROOT" "$MNT_LUSTRE" $ARCHIVE
+sh -x "$MOUNT_GFARM2FS_HSMTOOL" "$GFS_MOUNTDIR" "$GFARMFS_ROOT" "$GFS_USERNAME" "$GFARMFS_USERNAME" "$MNT_LUSTRE" "$HSM_ARCHIVE" "$HSM_OPTIONS"
